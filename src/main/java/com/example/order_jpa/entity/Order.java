b0d0b4c16fd081f2e8f56349081f9bfd825accc1
bkpark <bokyoungp@naver.com>
2023-11-08 12:39:49 +0900

    order_jpa v1.0

---
+++ src/main/java/com/example/order_jpa/entity/Order.java
@@ -0,0 +1,26 @@
+package com.example.order_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 지연로딩
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name="order_date", length = 10)
+    private String orderDate;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name="order_status", length = 10)
+    private OrderStatus orderStatus;
+    private double totalPrice;
+    private int totalQuantity;
+}
