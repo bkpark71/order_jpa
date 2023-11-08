d38db536acc649b90d19e8c20d48fa3308e215b8
bkpark <bokyoungp@naver.com>
2023-11-08 16:20:47 +0900

    orderjpa v2.0

--- src/main/java/com/example/order_jpa/entity/Order.java
+++ src/main/java/com/example/order_jpa/entity/Order.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/order_jpa/entity/Product.java
+++ src/main/java/com/example/order_jpa/entity/Product.java
@@ -12,6 +12,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long productId;
+    @Column(length = 45)
     private String name;
     private int price;
     private int quantity;
