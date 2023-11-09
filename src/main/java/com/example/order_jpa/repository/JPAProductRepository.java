5b97c78f5a509e4119b4820a8e1de9435e2382ea
bkpark <bokyoungp@naver.com>
2023-11-09 10:26:53 +0900

    order_jpa v3.0

--- src/main/java/com/example/order_jpa/repository/JPAProductRepository.java
+++ src/main/java/com/example/order_jpa/repository/JPAProductRepository.java
@@ -4,16 +4,21 @@ import com.example.order_jpa.entity.Product;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class JPAProductRepository {
     @PersistenceContext
     private final EntityManager em;
 
+    @Autowired
+    public JPAProductRepository(EntityManager em) {
+        this.em = em;
+    }
     public void save(Product product) {
         em.persist(product);
     }
@@ -26,7 +31,7 @@ public class JPAProductRepository {
         return em.find(Product.class, productId);
     }
 
-    public void delete(Product product) {
+    public void remove(Product product) {
         em.remove(product);
     }
 }
